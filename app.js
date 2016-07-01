1e9461cd6fbfd09792c6ebb445b81cb8b236e7b6
Connor O'Gara <ogara.connor@gmail.com>
2016-07-01 14:51:51 -0500

    finish, added delete functionality as well

---
+++ app.js
@@ -0,0 +1,55 @@
+$(function() {
+  var numRed = 0;
+  var numBlue = 0;
+  var numGreen = 0;
+  var numYellow = 0;
+
+  $('button').on('click',function() {
+    var tempColor = $(this).data('color');
+    $('.container').append('<div class="color-cube ' + tempColor + '" data-color="' + tempColor + '"></div>');
+    console.log(tempColor);
+    switch(tempColor) {
+      case 'red':
+        numRed++
+        $('#red span').text(numRed);
+        break;
+      case 'blue':
+        numBlue++
+        $('#blue span').text(numBlue);
+        break;
+      case 'green':
+        numGreen++
+        $('#green span').text(numGreen);
+        break;
+      case 'yellow':
+        numYellow++
+        $('#yellow span').text(numYellow);
+        break;
+    }
+  })
+
+  $('.container').on('click', '.color-cube', function() {
+    var tempColor = $(this).data('color');
+    $(this).remove();
+    switch(tempColor) {
+      case 'red':
+        numRed--;
+        $('#red span').text(numRed);
+        break;
+      case 'blue':
+        numBlue--;
+        $('#blue span').text(numBlue);
+        break;
+      case 'green':
+        numGreen--;
+        $('#green span').text(numGreen);
+        break;
+      case 'yellow':
+        numYellow--;
+        $('#yellow span').text(numYellow);
+        break;
+    }
+
+  })
+
+})
